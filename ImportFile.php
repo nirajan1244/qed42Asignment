8a229468c9b87dc9c704d8c8499a0e622f9a1b96
nirajan1244 <67497719+nirajan1244@users.noreply.github.com>
2022-12-22 13:15:22 +0530

    Add files via upload

---
+++ ImportFile.php
@@ -0,0 +1,69 @@
+namespace \Drupal\import\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;;
+
+class ImportFile extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'node_import_file';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['myfile'] = [
+      '#title' => $this->t('Upload myfile'),
+      '#type' => 'file',
+      // DO NOT PROVILDE '#required' => TRUE or your form will always fail validation!
+    ];
+    $form['submit'] = [
+    '#type' => 'submit',
+    '#value' => 'Import',
+    ];
+    return $form;
+  }
+  /**
+   * {@inheritdoc}
+   */
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    //$json = $form_state->getValue('paste');
+    $validators = ['file_validate_extensions' => ['json']];
+    $file = file_save_upload('myfile', $validators, FALSE, 0);
+    if (!$file) {
+      return;
+    }
+    else {
+      $data = file_get_contents($file->getFileUri());
+    }
+    $nodes = json_decode($data, TRUE);
+    $batch = [
+      'title' => t('Importing Nodes...'),
+      'operations' => [],
+      'init_message' => t('Imporitng'),
+      'progress_message' => t('Processed @current out of @total.'),
+      'error_message' => t('An error occurred during processing'),
+      'finished' => '\Drupal\node_export\NodeImport::nodeImportFinishedCallback',
+    ];
+    foreach ($nodes as $node) {
+      $batch['operations'][] = [
+        '\Drupal\node_export\Import::nodeImport',
+        [$node],
+      ];
+    }
+    batch_set($batch);
+    drupal_set_message(t('Node has been imported succesfully.'));
+  }
+}
